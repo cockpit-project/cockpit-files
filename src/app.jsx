9c0b1531c5d75f7dd7709a84c8e9c30b2e52326d
Mahmoud Hamdy <90795679+MahmoudHamdy02@users.noreply.github.com>
2023-05-10 13:21:39 +0300

    Add header contents & breadcrumbs to home page

--- src/app.jsx
+++ src/app.jsx
@@ -18,12 +18,13 @@
  */
 
 import cockpit from 'cockpit';
-import React from 'react';
-import { Flex, FlexItem, Icon, Page, PageSection } from "@patternfly/react-core";
-import { FileIcon, FolderIcon } from "@patternfly/react-icons";
+import React, { useState } from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, Card, CardBody, CardTitle, CardHeader, Dropdown, DropdownItem, DropdownList, Divider, Flex, FlexItem, Icon, MenuToggle, Page, PageBreadcrumb, PageSection, SearchInput } from "@patternfly/react-core";
+import { ArrowLeftIcon, ArrowRightIcon, EditAltIcon, EllipsisVIcon, FileIcon, FolderIcon, ListIcon, PficonHistoryIcon, StarIcon } from "@patternfly/react-icons";
 
 const _ = cockpit.gettext;
 
+// To do: convert to functional component
 export class Application extends React.Component {
     constructor() {
         super();
@@ -41,37 +42,161 @@ export class Application extends React.Component {
                 .then(() => {
                     cockpit.spawn(["ls", "-p", `/home/${this.state.current_user}`], { superuser: true }).then((res) => {
                         res = res.split("\n");
-                        console.log(res.slice(0, res.length - 1));
                         this.setState({ files: res.slice(0, res.length - 1) });
                     });
                 });
     }
 
+    // To do: button spacing, correct refresh icon, dropdown
     render() {
         return (
             <Page>
-                <PageSection>
+                <PageBreadcrumb stickyOnBreakpoint={{ default: "top" }}>
                     <Flex>
-                        {this.state.files.map((file) => {
-                            const directory = file.substring(file.length - 1) === "/";
-                            if (directory)
-                                file = file.substring(0, file.length - 1);
-                            return (
-                                <Flex key={file} direction={{ default: "column" }} spaceItems={{ default: 'spaceItemsNone' }}>
-                                    <FlexItem alignSelf={{ default: "alignSelfCenter" }}>
-                                        <Icon size="xl">
-                                            {directory
-                                                ? <FolderIcon />
-                                                : <FileIcon />}
-                                        </Icon>
-                                    </FlexItem>
-                                    <FlexItem alignSelf={{ default: "alignSelfCenter" }}>{file}</FlexItem>
-                                </Flex>
-                            );
-                        })}
+                        <FlexItem>
+                            <Button variant="secondary">
+                                <Icon>
+                                    <ArrowLeftIcon />
+                                </Icon>
+                            </Button>
+                        </FlexItem>
+                        <FlexItem>
+                            <Button variant="secondary">
+                                <Icon>
+                                    <ArrowRightIcon />
+                                </Icon>
+                            </Button>
+                        </FlexItem>
+                        <FlexItem>
+                            <Button variant="secondary">
+                                <Icon>
+                                    <PficonHistoryIcon />
+                                </Icon>
+                            </Button>
+                        </FlexItem>
+                        <FlexItem>
+                            <Breadcrumb>
+                                <BreadcrumbItem to="#/">{_("home")}</BreadcrumbItem>
+                            </Breadcrumb>
+                        </FlexItem>
+                        <FlexItem align={{ default: 'alignRight' }}>
+                            <Button variant="secondary">
+                                <Icon>
+                                    <EditAltIcon />
+                                </Icon>
+                            </Button>
+                        </FlexItem>
+                        <FlexItem>
+                            <Button variant="secondary">
+                                <Icon>
+                                    <StarIcon />
+                                </Icon>
+                            </Button>
+                        </FlexItem>
+                        <FlexItem>
+                            <DropdownWithKebab />
+                        </FlexItem>
                     </Flex>
+                </PageBreadcrumb>
+                <PageSection>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle component="h2">{_("Directories & files")}</CardTitle>
+                            <Flex flexWrap={{ default: 'nowrap' }}>
+                                <SearchInput placeholder={_("Filter directory")} />
+                                <ViewSelector />
+                                <Button variant="secondary">Upload</Button>
+                                <FlexItem>
+                                    <DropdownWithKebab />
+                                </FlexItem>
+                            </Flex>
+                        </CardHeader>
+                        <CardBody>
+                            <Flex id="folder-view">
+                                {this.state.files.map((file) => {
+                                    const directory = file.substring(file.length - 1) === "/";
+                                    if (directory)
+                                        file = file.substring(0, file.length - 1);
+                                    return (
+                                        <Flex key={file} direction={{ default: "column" }} spaceItems={{ default: 'spaceItemsNone' }}>
+                                            <FlexItem alignSelf={{ default: "alignSelfCenter" }}>
+                                                <Icon size="xl">
+                                                    {directory
+                                                        ? <FolderIcon />
+                                                        : <FileIcon />}
+                                                </Icon>
+                                            </FlexItem>
+                                            <FlexItem alignSelf={{ default: "alignSelfCenter" }}>{file}</FlexItem>
+                                        </Flex>
+                                    );
+                                })}
+                            </Flex>
+                        </CardBody>
+                    </Card>
                 </PageSection>
             </Page>
         );
     }
 }
+
+const DropdownWithKebab = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const onToggleClick = () => {
+        setIsOpen(!isOpen);
+    };
+    const onSelect = (_event, itemId) => {
+        setIsOpen(false);
+    };
+    return (
+        <Dropdown
+            isPlain
+            isOpen={isOpen}
+            onSelect={onSelect}
+            onOpenChange={isOpen => setIsOpen(isOpen)}
+            toggle={toggleRef =>
+                <MenuToggle ref={toggleRef} aria-label="kebab dropdown toggle" variant="plain" onClick={onToggleClick} isExpanded={isOpen}>
+                    <EllipsisVIcon />
+                </MenuToggle>}
+        >
+            <DropdownList>
+                <DropdownItem itemId={0} key="action1">
+                    Action
+                </DropdownItem>
+                <DropdownItem itemId={1} key="action2">
+                    Action
+                </DropdownItem>
+                <Divider component="li" key="separator" />
+                <DropdownItem itemId={2} key="action3">
+                    Action
+                </DropdownItem>
+            </DropdownList>
+        </Dropdown>
+    );
+};
+
+export const ViewSelector = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const onToggleClick = isOpen => setIsOpen(!isOpen);
+    const onSelect = () => setIsOpen(false);
+    return (
+        <Dropdown
+            isOpen={isOpen}
+            onSelect={onSelect}
+            onOpenChange={setIsOpen}
+            toggle={toggleRef => <MenuToggle ref={toggleRef} onClick={() => { onToggleClick(isOpen) }} isExpanded={isOpen} variant="secondary" splitButtonOptions={{ variant: "action", items: [<Icon key="list-icon"><ListIcon /></Icon>] }} />}
+        >
+            <DropdownList>
+                <DropdownItem itemId={0} key="action1">
+                    Action
+                </DropdownItem>
+                <DropdownItem itemId={1} key="action2">
+                    Action
+                </DropdownItem>
+                <Divider component="li" key="separator" />
+                <DropdownItem itemId={2} key="action3">
+                    Action
+                </DropdownItem>
+            </DropdownList>
+        </Dropdown>
+    );
+};
