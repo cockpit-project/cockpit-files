47c9e3b4c55facb8bd28a9af70e56efd4b6fb63d
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-06-19 12:16:59 +0300

    Change test CSS selectors to correctly select last breadcrumb element

--- src/app.jsx
+++ src/app.jsx
@@ -167,7 +167,7 @@ const NavigatorBreadcrumbs = ({ path, setPath, pathIndex, setPathIndex }) => {
                                 <React.Fragment key={dir}>
                                     {i !== path.slice(0, pathIndex).length - 1
                                         ? <Button variant='link' onClick={() => { navigateBreadcrumb(i + 1) }} key={dir} className='breadcrumb-button'>{dir}</Button>
-                                        : <p>{dir}</p>}
+                                        : <p className='last-breadcrumb-button'>{dir}</p>}
                                     <p key={i}>/</p>
                                 </React.Fragment>
                             );
