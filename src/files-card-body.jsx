cockpit-project/cockpit-files

commit e1664765bc8de371dbb9dad64c71625e701bead0

Show icons for various different file types
Create a new esbuild plugin to compile data from various sources,
creating an efficient map of filename extensions to file type
information.

The most important piece of the filetype information at present, is a
"class" attribute which is used to add an extra class to the `<tr>`
containing the file in the list or grid views.  In turn, we add a number
of icons for the various classes to our stylesheet.

This gets us special icons for "archive", "audio", "source code",
"image", "text" and "video" files.

The file type classification is done entirely on the basis of the
filename extension.  More specifically: there is no content sniffing.

Closes #226
Closes #480

src/files-card-body.jsx
@@ -402,6 +402,8 @@ export const FilesCardBody = ({
 const getFileType = (file) => {
     if (file.to === "dir") {
         return "folder";
+    } else if (file.category.class) {
+        return file.category.class;
     } else {
         return "file";
     }

build.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env -S node --import tsx/esm
 
 import fs from 'node:fs';
 import path from 'node:path';
@@ -11,6 +11,7 @@ import { cockpitCompressPlugin } from './pkg/lib/esbuild-compress-plugin.js';
 import { cockpitPoEsbuildPlugin } from './pkg/lib/cockpit-po-plugin.js';
 import { cockpitRsyncEsbuildPlugin } from './pkg/lib/cockpit-rsync-plugin.js';
 import { esbuildStylesPlugins } from './pkg/lib/esbuild-common.js';
+import { filetype_plugin } from './src/filetype-plugin';
 
 const useWasm = os.arch() !== 'x64';
 const esbuild = (await import(useWasm ? 'esbuild-wasm' : 'esbuild')).default;
@@ -100,6 +101,7 @@ const context = await esbuild.context({
                 { from: ['./src/index.html'], to: ['./index.html'] },
             ]
         }),
+        filetype_plugin,
         ...esbuildStylesPlugins,
         cockpitPoEsbuildPlugin(),
         ...production ? [cockpitCompressPlugin()] : [],

src/app.tsx
@@ -35,6 +35,9 @@ import { FilesFolderView } from "./files-folder-view";
 import { usePageLocation } from "hooks.js";
 import { fsinfo, FileInfo } from "./fsinfo";
 
+import { CategoryMetadata, filetype_lookup } from './filetype-lookup';
+import filetype_data from './filetype-data';
+
 superuser.reload_page_on_change();
 
 interface Alert {
@@ -47,6 +50,7 @@ interface Alert {
 export interface FolderFileInfo extends FileInfo {
     name: string,
     to: string | null,
+    category: CategoryMetadata | null,
 }
 
 interface FilesContextType {
@@ -99,17 +103,18 @@ export const Application = () => {
                 `/${currentPath}`,
                 ["type", "mode", "size", "mtime", "user", "group", "target", "entries", "targets"]
             );
+
             return info.effect(state => {
                 setLoading(false);
                 setLoadingFiles(!(state.info || state.error));
                 setCwdInfo(state.info);
                 setErrorMessage(state.error?.message ?? "");
                 const entries = Object.entries(state?.info?.entries || {});
-                const files = entries.map(([name, attrs]) => ({
-                    ...attrs,
-                    name,
-                    to: info.target(name)?.type ?? null
-                }));
+                const files = entries.map(([name, attrs]) => {
+                    const to = info.target(name)?.type ?? null;
+                    const category = to === 'reg' ? filetype_lookup(filetype_data, name) : null;
+                    return { ...attrs, name, to, category };
+                });
                 setFiles(files);
             });
         },

src/files-card-body.scss
@@ -70,6 +70,42 @@
         }
     }
 
+    tr.archive-file {
+        .item-name a::before {
+            mask-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1536 1792"><path d="M640 384V256H512v128zm128 128V384H640v128zM640 640V512H512v128zm128 128V640H640v128zm700-388c37 37 68 111 68 164v1152c0 53-43 96-96 96H96c-53 0-96-43-96-96V96C0 43 43 0 96 0h896c53 0 127 31 164 68zm-444-244v376h376c-6-17-15-34-22-41l-313-313c-7-7-24-16-41-22m384 1528V640H992c-53 0-96-43-96-96V128H768v128H640V128H128v1536zM781 943c85 287 107 349 107 349 5 17 8 34 8 52 0 111-108 192-256 192s-256-81-256-192c0-18 3-35 8-52 0 0 21-62 120-396V768h128v128h79c29 0 54 19 62 47m-141 465c71 0 128-29 128-64s-57-64-128-64-128 29-128 64 57 64 128 64"/></svg>');
+        }
+    }
+
+    tr.audio-file {
+        .item-name a::before {
+            mask-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1536 1792"><path d="M1468 380c37 37 68 111 68 164v1152c0 53-43 96-96 96H96c-53 0-96-43-96-96V96C0 43 43 0 96 0h896c53 0 127 31 164 68zm-444-244v376h376c-6-17-15-34-22-41l-313-313c-7-7-24-16-41-22m384 1528V640H992c-53 0-96-43-96-96V128H128v1536zM620 850c12 5 20 17 20 30v544c0 13-8 25-20 30-4 1-8 2-12 2-8 0-16-3-23-9l-166-167H288c-18 0-32-14-32-32v-192c0-18 14-32 32-32h131l166-167c10-9 23-12 35-7m417 689c19 0 37-8 50-24 83-102 129-231 129-363s-46-261-129-363c-22-28-63-32-90-10-28 23-32 63-9 91 65 80 100 178 100 282s-35 202-100 282c-23 28-19 68 9 90 12 10 26 15 40 15m-211-148c17 0 34-7 47-20 56-60 87-137 87-219s-31-159-87-219c-24-26-65-27-91-3-25 24-27 65-2 91 33 36 52 82 52 131s-19 95-52 131c-25 26-23 67 2 91 13 11 29 17 44 17"/></svg>');
+        }
+    }
+
+    tr.code-file {
+        .item-name a::before {
+            mask-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1536 1792"><path d="M1468 380c37 37 68 111 68 164v1152c0 53-43 96-96 96H96c-53 0-96-43-96-96V96C0 43 43 0 96 0h896c53 0 127 31 164 68zm-444-244v376h376c-6-17-15-34-22-41l-313-313c-7-7-24-16-41-22m384 1528V640H992c-53 0-96-43-96-96V128H128v1536zM480 768c11-14 31-17 45-6l51 38c14 11 17 31 6 45l-182 243 182 243c11 14 8 34-6 45l-51 38c-14 11-34 8-45-6l-226-301c-8-11-8-27 0-38zm802 301c8 11 8 27 0 38l-226 301c-11 14-31 17-45 6l-51-38c-14-11-17-31-6-45l182-243-182-243c-11-14-8-34 6-45l51-38c14-11 34-8 45 6zm-620 461c-18-3-29-20-26-37l138-831c3-18 20-29 37-26l63 10c18 3 29 20 26 37l-138 831c-3 18-20 29-37 26z"/></svg>');
+        }
+    }
+
+    tr.image-file {
+        .item-name a::before {
+            mask-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1536 1792"><path d="M1468 380c37 37 68 111 68 164v1152c0 53-43 96-96 96H96c-53 0-96-43-96-96V96C0 43 43 0 96 0h896c53 0 127 31 164 68zm-444-244v376h376c-6-17-15-34-22-41l-313-313c-7-7-24-16-41-22m384 1528V640H992c-53 0-96-43-96-96V128H128v1536zm-128-448v320H256v-192l192-192 128 128 384-384zm-832-192c-106 0-192-86-192-192s86-192 192-192 192 86 192 192-86 192-192 192"/></svg>');
+        }
+    }
+
+    tr.text-file {
+        .item-name a::before {
+            mask-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1536 1792"><path d="M1468 380c37 37 68 111 68 164v1152c0 53-43 96-96 96H96c-53 0-96-43-96-96V96C0 43 43 0 96 0h896c53 0 127 31 164 68zm-444-244v376h376c-6-17-15-34-22-41l-313-313c-7-7-24-16-41-22m384 1528V640H992c-53 0-96-43-96-96V128H128v1536zM384 800c0-18 14-32 32-32h704c18 0 32 14 32 32v64c0 18-14 32-32 32H416c-18 0-32-14-32-32zm736 224c18 0 32 14 32 32v64c0 18-14 32-32 32H416c-18 0-32-14-32-32v-64c0-18 14-32 32-32zm0 256c18 0 32 14 32 32v64c0 18-14 32-32 32H416c-18 0-32-14-32-32v-64c0-18 14-32 32-32z"/></svg>');
+        }
+    }
+
+    tr.video-file {
+        .item-name a::before {
+            mask-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1536 1792"><path d="M1468 380c37 37 68 111 68 164v1152c0 53-43 96-96 96H96c-53 0-96-43-96-96V96C0 43 43 0 96 0h896c53 0 127 31 164 68zm-444-244v376h376c-6-17-15-34-22-41l-313-313c-7-7-24-16-41-22m384 1528V640H992c-53 0-96-43-96-96V128H128v1536zM768 768c70 0 128 58 128 128v384c0 70-58 128-128 128H384c-70 0-128-58-128-128V896c0-70 58-128 128-128zm492 2c12 5 20 17 20 30v576c0 13-8 25-20 30-4 1-8 2-12 2-8 0-17-3-23-9l-265-266v-90l265-266c6-6 15-9 23-9 4 0 8 1 12 2"/></svg>');
+        }
+    }
+
     a:not(:hover, :focus) {
         color: inherit;
     }

src/filetype-data.d.ts
@@ -1,0 +1,5 @@
+import { FileTypeData } from './filetype-lookup';
+
+/* This data comes dynamically as JSON from filetype-plugin.ts */
+declare const filetype_data: FileTypeData;
+export default filetype_data;

src/filetype-lookup.ts
@@ -1,0 +1,38 @@
+export enum Category {
+    FILE = 0,
+
+    ARCHIVE,
+    AUDIO,
+    CODE,
+    IMAGE,
+    TEXT,
+    VIDEO,
+}
+
+export interface CategoryMetadata {
+    name: string;
+    class: string;
+}
+
+export interface FileTypeData {
+    categories: Record<Category, CategoryMetadata>;
+    extensions: Record<string, Category>;
+    max_extension_length: number;
+}
+
+export function filetype_lookup(filetype_data: FileTypeData, name: string) {
+    // Never find a dot at offset '0' (the one for a hidden file), or one that
+    // would produce an extension that we know not to be in the database.
+    let offset = Math.max(1, name.length - filetype_data.max_extension_length - 1);
+
+    // Allow finding extensions like '.tar.gz' (prefer longest first)
+    while ((offset = name.indexOf('.', offset)) > 0) {
+        offset++;
+        const ext = name.substring(offset);
+        if (ext in filetype_data.extensions) {
+            return filetype_data.categories[filetype_data.extensions[ext]];
+        }
+    }
+
+    return filetype_data.categories[Category.FILE];
+}

src/filetype-plugin.ts
@@ -1,0 +1,114 @@
+/* This is an esbuild plugin that creates a JSON blob mapping extensions to
+ * file types at bundling time.
+ *
+ * We do this instead of including the raw information because the raw
+ * information contains a lot of things we don't need and is structured in a
+ * way that makes it difficult to perform the lookup we're interested in:
+ * mapping extension to file type.
+ *
+ * We use mime-db for images, audio, video, and text.  In order to distinguish
+ * source code from other kinds of text, we use the 'language-map' data, which
+ * is the same database GitHub uses to show which languages are in use in a
+ * repository.  Archives are not handled well by either of those sources, so we
+ * include our own list (scraped from Wikipedia).
+ *
+ * The `create_filetype_data()` function returns the data in the correct
+ * format.  The `FileTypePlugin` is responsible for making sure that this data
+ * is available to anyone who does an `import` of `./filetype-data`.  This is
+ * done as a loader, but instead of loading the data from disk, it's computed.
+ * That means that the data will end up in the bundle, but is never in the src/
+ * directory.
+ *
+ * The lookup side of this all lives in 'filetype-lookup.ts'.  It can be used
+ * with the data imported from './filetype-data' or it can be used directly
+ * with the return value of the `create_filetype_data()` function`.
+ */
+
+import { Plugin, PluginBuild } from 'esbuild';
+import { Category, FileTypeData } from './filetype-lookup';
+
+import language_map from 'language-map';
+import mime_db from 'mime-db/db.json';
+
+export function create_filetype_data() : FileTypeData {
+    const categories = {
+        [Category.FILE]: { name: "Unknown type", class: "file" },
+        [Category.ARCHIVE]: { name: "Archive file", class: "archive-file" },
+        [Category.AUDIO]: { name: "Audio file", class: "audio-file" },
+        [Category.CODE]: { name: "Source code file", class: "code-file" },
+        [Category.IMAGE]: { name: "Image file", class: "image-file" },
+        [Category.TEXT]: { name: "Text file", class: "text-file" },
+        [Category.VIDEO]: { name: "Video file", class: "video-file" },
+    } as const;
+
+    const extensions: { [ext: string]: Category } = {};
+    let max_extension_length = 0;
+
+    function add_category_extensions(category: Category, exts: string[]) {
+        for (let ext of exts) {
+            if (ext[0] === '.') {
+                ext = ext.substring(1);
+            }
+
+            max_extension_length = Math.max(max_extension_length, ext.length);
+            extensions[ext] = category;
+        }
+    }
+
+    // Broad-stroke categorization based on 'mime-db' package
+    for (const [mimetype, details] of Object.entries(mime_db)) {
+        if ('extensions' in details) {
+            if (mimetype.startsWith('image/')) {
+                add_category_extensions(Category.IMAGE, details.extensions);
+            } else if (mimetype.startsWith('audio/')) {
+                add_category_extensions(Category.AUDIO, details.extensions);
+            } else if (mimetype.startsWith('video/')) {
+                add_category_extensions(Category.VIDEO, details.extensions);
+            } else if (mimetype.startsWith('text/')) {
+                add_category_extensions(Category.TEXT, details.extensions);
+            }
+        }
+    }
+
+    // Archives, scraped from https://en.wikipedia.org/wiki/List_of_archive_formats
+    add_category_extensions(
+        Category.ARCHIVE,
+        [
+            '7z', 'F', 'LBR', 'Z', 'a', 'aar', 'ace', 'afa', 'alz', 'apk', 'ar', 'arc', 'arc', 'arj', 'ark',
+            'b1', 'b6z', 'ba', 'bh', 'br', 'bz2', 'cab', 'car', 'cdx', 'cfs', 'cpio', 'cpt', 'dar', 'dd',
+            'dgc', 'ear', 'gca', 'genozip', 'genozip', 'gz', 'ha', 'hki', 'ice', 'iso', 'jar', 'kgb', 'lbr',
+            'lha', 'lz', 'lz4', 'lzh', 'lzma', 'lzo', 'lzx', 'mar', 'pak', 'paq6', 'paq7', 'paq8', 'partimg',
+            'pea', 'phar', 'pim', 'pit', 'qda', 'rar', 'rk', 'rz', 's7z', 'sbx', 'sda', 'sea', 'sen', 'sfark',
+            'sfx', 'shar', 'shk', 'sit', 'sitx', 'sqx', 'sz', 'tar', 'tar.Z', 'tar.bz2', 'tar.gz', 'tar.lz',
+            'tar.xz', 'tar.zst', 'tbz2', 'tgz', 'tlz', 'txz', 'uc', 'uc0', 'uc2', 'uca', 'ucn', 'ue2', 'uha',
+            'ur2', 'war', 'wim', 'xar', 'xp3', 'xz', 'yz1', 'z', 'zip', 'zipx', 'zoo', 'zpaq', 'zst', 'zz'
+        ]
+    );
+
+    // Special treatment for programming languages based on GitHub's database
+    for (const lang of Object.values(language_map)) {
+        if (lang.type === 'programming' && 'extensions' in lang) {
+            add_category_extensions(Category.CODE, lang.extensions);
+        }
+    }
+
+    return { categories, extensions, max_extension_length };
+}
+
+export class FileTypePlugin implements Plugin {
+    name = 'cockpit-files-filetype-plugin';
+
+    setup(build: PluginBuild) {
+        build.onResolve({ filter: /^\.\/filetype-data$/ }, args => ({
+            path: args.path,
+            namespace: 'cockpit-files-filetype-plugin',
+        }));
+
+        build.onLoad({ filter: /.*/, namespace: 'cockpit-files-filetype-plugin' }, () => ({
+            contents: JSON.stringify(create_filetype_data()),
+            loader: 'json',
+        }));
+    }
+}
+
+export const filetype_plugin = new FileTypePlugin();

commit 29416239b049ed5939f907f992a8775c52acbb08

Don't crash on special files
Fixes a bug introduced in e1664765bc8de371dbb9dad64c71625e701bead0.

The file category is only present on regular files, but we access it
unconditionally on anything that isn't a directory.  That causes crashes
when entering /dev or anywhere else that contains devices, sockets, or
FIFOs.

Make the access conditional and add a test for entering /dev.

Fixes #507

src/files-card-body.jsx
@@ -402,7 +402,7 @@ export const FilesCardBody = ({
 const getFileType = (file) => {
     if (file.to === "dir") {
         return "folder";
-    } else if (file.category.class) {
+    } else if (file.category?.class) {
         return file.category.class;
     } else {
         return "file";

test/check-application
@@ -192,6 +192,10 @@ class TestFiles(testlib.MachineCase):
         b.click(".breadcrumb-button:nth-of-type(2)")
         b.wait_visible("[data-item='home']")
 
+        # Enter /dev to make sure we can show special files properly
+        b.mouse("[data-item='dev']", "dblclick")
+        b.wait_visible("[data-item='urandom']")
+
         # Non-existing directory
         b.go("/files#/?path=/doesnotexists")
         b.wait_in_text(".pf-v5-c-empty-state__body", "No such file or directory")