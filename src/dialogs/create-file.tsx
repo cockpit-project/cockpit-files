9fa57d0f562166ff3f0edfaecf908e2345099c76
Jelle van der Waa <jvanderwaa@redhat.com>
2025-04-10 17:17:18 +0200

    Use `fsinfo` to obtain the file tag
    Compared to `fsread1` we no longer read the file contents making this
    work out of the box when Cockpit re-adds the max_read_size limit and
    this should be a small performance win.
    
    Relates: #1027

--- src/dialogs/create-file.tsx
+++ src/dialogs/create-file.tsx
@@ -31,6 +31,7 @@ import { TextInput } from "@patternfly/react-core/dist/esm/components/TextInput"
 import { Stack } from '@patternfly/react-core/dist/esm/layouts/Stack';
 
 import cockpit from 'cockpit';
+import { fsinfo } from 'cockpit/fsinfo.ts';
 import { FormHelper } from 'cockpit-components-form-helper';
 import type { Dialogs, DialogResult } from 'dialogs';
 import { useInit } from 'hooks';
@@ -68,21 +69,18 @@ async function create_file(filename: string, content?: string, owner?: string | 
 
     if (content) {
         // We need to obtain the tag to retain file ownership
-        // Can't do this async because we can't get the tag via await
-        await cockpit.file(filename, { superuser: "try" }).read()
-                .then(((async (_content: string, tag: string) => {
-                    try {
-                        await cockpit.file(filename, { superuser: "try" }).replace(content, tag);
-                    } catch (err) {
-                        console.warn("Cannot set initial file text", err);
-                        try {
-                            await cockpit.spawn(["rm", filename], { superuser: "require" });
-                        } catch (err) {
-                            console.warn(`Failed to cleanup ${filename}`, err);
-                        }
-                        throw err;
-                    }
-                }) as any /* eslint-disable-line @typescript-eslint/no-explicit-any */));
+        const { tag } = await fsinfo(filename, ['tag'], { superuser: "try" });
+        try {
+            await cockpit.file(filename, { superuser: "try" }).replace(content, tag);
+        } catch (err) {
+            console.warn("Cannot set initial file text", err);
+            try {
+                await cockpit.spawn(["rm", filename], { superuser: "require" });
+            } catch (err) {
+                console.warn(`Failed to cleanup ${filename}`, err);
+            }
+            throw err;
+        }
     }
 }
 
