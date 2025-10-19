08f2710ffcd05e4a07dc54403f3801c2c67bd52a
Martin Pitt <martin@piware.de>
2025-10-19 19:54:34 +0200

    Ship npm integration test dependencies in dist tarball
    This avoids having to run `npm`, and thus a potentially brittle internet
    access, during FMF test runs. It also avoids the npm test dependency,
    which tends to be uninstallable in Rawhide.
    
    We have successfully used this approach in cockpit for a long time.
    
    Taken from https://github.com/cockpit-project/cockpit-podman/commit/71f80a0acfc6

--- test/browser/run-test.sh
+++ test/browser/run-test.sh
@@ -7,18 +7,14 @@ git init
 rm -f bots  # common local case: existing bots symlink
 make bots test/common
 
-# support running from clean git tree
-if [ ! -d node_modules/chrome-remote-interface ]; then
-    # copy package.json temporarily otherwise npm might try to install the dependencies from it
-    rm -f package-lock.json  # otherwise the command below installs *everything*, argh
-    mv package.json .package.json
-    # only install a subset to save time/space
-    npm install chrome-remote-interface sizzle
-    mv .package.json package.json
+if [ -e .git ]; then
+    tools/node-modules checkout
+    # disable detection of affected tests; testing takes too long as there is no parallelization
+    mv .git dot-git
+else
+    # upstream tarballs ship test dependencies; print version for debugging
+    grep '"version"' node_modules/sizzle/package.json
 fi
-
-# disable detection of affected tests; testing takes too long as there is no parallelization
-mv .git dot-git
 
 . /run/host/usr/lib/os-release
 export TEST_OS="${ID}-${VERSION_ID/./-}"
